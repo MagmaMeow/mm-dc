a56f6bac71cf4e145de7456c521cfa7eb0284f9e
Parker-Bettridge <parker25010@rangitikeicollege.school.nz>
2025-09-12 00:39:02 +1200

    Update bot.py

--- modmail_bot/bot.py
+++ modmail_bot/bot.py
@@ -36,7 +36,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix=".", intents=intents)
 tree = bot.tree
 
 # --- In-memory user-thread mapping ---
